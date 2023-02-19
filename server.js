c00fbc0fbcce0af2714570c90a52f0d26de8c313
Josh Icard <joshicard@Joshs-MacBook-Pro.local>
2023-02-19 13:20:23 -0500

    connected to db

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import User from "./schemas/User.js"
+
 
 const app = express();
+mongoose.connect(process.env.databaseLink);
+const port = process.env.port;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static("public"));
+//app.use(require"./routes");
 
 app.use("/", (req, res, next) => {
     res.send("Hello world")
 })
 
-app.listen(3001);
+
+app.listen(port);
